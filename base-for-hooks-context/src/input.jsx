397875898ec5b2ea347053ed3d8d1c3bf8933d2c
Rachel <rachel17.22@gmail.com>
2020-05-19 14:10:35 -0600

    Basic Input component

---
+++ base-for-hooks-context/src/input.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Input() {
+  return (
+    <div data-test="component-input">
+    </div>
+  );
+};
+
+export default Input;
--- base-for-hooks-context/src/input.test.js
+++ base-for-hooks-context/src/input.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
-import App from './App';
+import Input from './input';
 
 /**
- * Setup function for app component
+ * Setup function for input component
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return shallow(<Input />);
 };
 
-test('App renders without error', () => {
+test('Input renders without error', () => {
   const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-app');
+  const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
 });
